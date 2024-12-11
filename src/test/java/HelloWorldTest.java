87091ec02a8c59fea35fe2e58bbc9f865a0dce3a
dimon <dimon.ag6@gmail.com>
2024-12-11 17:14:07 +0300

    HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -8,6 +8,7 @@ public class HelloWorldTest {
     public void testHelloWorld(){
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/hello")
+
                 .andReturn();
         response.prettyPrint();
     }
